9462f84d5bac4a24c00607d2c9228f92aa58203e
Purvi Kanal <purvikanal92@gmail.com>
2017-06-28 13:14:24 -0400

    add flow to app container

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,20 @@
+// @flow
 import React from 'react';
 import './app.css';
 
+type Tile = {
+  x: number,
+  y: number,
+  colour: string,
+};
+
+type Props = {
+  tiles: Tile[],
+};
+
 const App = ({
   tiles,
-}) => {
+}: Props) => {
   return (
     <div className='container'>
       <div className='tetris-grid'>
